80347d6010328c1ffb0e079a3a1130b795f7e80e
tsp0104 <tsp0104@126.com>
2019-10-28 21:39:08 +0800

    整体页面基本完成

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,9 @@
-// 导出axios给main挂载
+// 导出一个配置好的axios提供给main挂载
 import axios from 'axios'
 
 // 对axios进行配置
-// baseURL:设置基准地址(以后不用写前面相同的地址，只要写后面的就可以)
+// baseURL  作用：设置基准地址（前面一段相同的地址）
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
-
-// 其它配置(暂时没有)
+// 其他配置 ...
 
 export default axios
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,28 @@
-import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Vue from 'vue'
+
 import Login from '@/views/login'
 import Home from '@/views/home'
+import Welcome from '@/views/welcome'
+import NotFound from '@/views/404'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
+    // 登录
+    { path: '/login', component: Login },
+    // 首页
     {
-      path: '/login',
-      component: Login
+      path: '/',
+      component: Home,
+      children: [
+        // 欢迎
+        { path: '/', component: Welcome }
+      ]
     },
-    {
-      path: '/home',
-      component: Home
-    }
+    // 404处理
+    { path: '*', component: NotFound }
   ]
 })
 
